3681d8af71362feac6fa43a13bcd687601ea6f52
Sergei Bagautdinov <s.bagautdinov@yandex.ru>
2021-04-12 21:41:09 +0300

    homeWork_3 upgraded Добавлена функция подчета слов

--- JavaCore/homeWork_3/src/geekbrains/study/Main.java
+++ JavaCore/homeWork_3/src/geekbrains/study/Main.java
@@ -4,16 +4,17 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
 
 public class Main {
 
     static final String FILE_NAME = "resources\\textFile.txt";
+    static final int PLUS_WORD = 1;
 
     public static void main(String[] args) {
         System.out.println(Arrays.toString(getStringArray()));
+        System.out.println("-------------------------------------------------");
+        wordCounter(getStringArray());
     }
 
     private static String[] getStringArray() {
@@ -32,5 +33,18 @@ public class Main {
         return strings;
     }
 
+    private static void wordCounter(String[] strings) {
+        Integer number;
+        Map<String, Integer> hashMap = new HashMap<>();
+        for (String word : strings) {
+            number = hashMap.get(word);
+            hashMap.put(word, ((number == null) ? PLUS_WORD : number + PLUS_WORD));
+            // вторая реализация
+            //hm.put(word, hashMap.getOrDefault(word, 0) + PLUS_WORD);
+        }
+        System.out.println(hashMap);
+        System.out.println("В массиве " + strings.length + " слова " +
+                hashMap.size() + " неповторяющихся слов");
+    }
     // Допиливаю...
 }
