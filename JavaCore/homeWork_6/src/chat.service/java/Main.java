07358b99187f90d355d0a857fa888a9aecc73912
Sergei Bagautdinov <s.bagautdinov@yandex.ru>
2021-04-22 22:53:02 +0300

    Code is upgraded

---
+++ JavaCore/homeWork_6/src/chat.service/java/Main.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("chat.fxml")));
+        primaryStage.setTitle("Сетевой чат 1.0.0");
+        primaryStage.setScene(new Scene(root, 350, 700));
+        primaryStage.show();
+    }
+}
