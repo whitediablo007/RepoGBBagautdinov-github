8a0f889c2ffac70add9bcf29f161fdc9bb74a705
Sergei Bagautdinov <s.bagautdinov@yandex.ru>
2021-04-23 14:43:02 +0300

    task is realized - code is upgraded

--- JavaCore/homeWork_6/src/chat.service/java/host/Server.java
+++ JavaCore/homeWork_6/src/chat.service/java/host/Server.java
@@ -8,7 +8,7 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Server {
-    @SuppressWarnings("InfiniteLoopStatement")
+
     public static void main(String[] args) {
         Socket socket;
         try (ServerSocket serverSocket = new ServerSocket(8112)) {
@@ -17,28 +17,36 @@ public class Server {
             DataInputStream inputStream = new DataInputStream(socket.getInputStream());
             DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
 
-            new Thread(() -> {
-                try {
-                    while (true) {
-                        String string = inputStream.readUTF();
-                        System.out.println(string);
-                    }
-                } catch (Exception exception) {
-                    exception.printStackTrace();
-                }
-            }).start();
-            try {
+            addNewThread(inputStream);
+            sendFromServerConsole(outputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    private static void addNewThread(DataInputStream inputStream) {
+        new Thread(() -> {
+            try {
                 while (true) {
-                    Scanner scanner = new Scanner(System.in);
-                    String message = scanner.nextLine();
-                    outputStream.writeUTF(message);
+                    String string = inputStream.readUTF();
+                    System.out.println(string);
                 }
             } catch (Exception exception) {
                 exception.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        }).start();
+    }
+
+    @SuppressWarnings("InfiniteLoopStatement")
+    private static void sendFromServerConsole(DataOutputStream outputStream) {
+        try {
+            while (true) {
+                Scanner scanner = new Scanner(System.in);
+                String message = scanner.nextLine();
+                outputStream.writeUTF(message);
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
         }
     }
 }
