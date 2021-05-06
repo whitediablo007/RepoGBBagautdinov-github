ccaa22c55e1ca1e03e158edeaa6043de48060e55
Sergei Bagautdinov <s.bagautdinov@yandex.ru>
2021-05-06 19:04:08 +0300

    upgraded

--- Java3/homeWork_2/src/geekbrains/study/Main.java
+++ Java3/homeWork_2/src/geekbrains/study/Main.java
@@ -3,11 +3,7 @@ package geekbrains.study;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello worfsgfgs ld!");
-
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Hello worfsgfgs ld!");
-        }
+        // There will be something...
 
     }
 }
