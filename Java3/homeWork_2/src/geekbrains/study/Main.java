396e23a7f2e4cfdbb03874d3a582cecfb26394fd
Sergei Bagautdinov <s.bagautdinov@yandex.ru>
2021-05-06 16:54:27 +0300

    HW_3 created

--- Java3/homeWork_2/src/geekbrains/study/Main.java
+++ Java3/homeWork_2/src/geekbrains/study/Main.java
@@ -3,6 +3,11 @@ package geekbrains.study;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello worfsgfgs ld!");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello worfsgfgs ld!");
+        }
+
     }
 }
